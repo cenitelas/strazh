e30e424fcb3731782e5565a2d6f731adbf619b9e
fivern <cenitelas1@gmail.com>
2020-06-03 03:59:38 +0600

    +

--- src/Components/Cell/Cell.jsx
+++ src/Components/Cell/Cell.jsx
@@ -4,6 +4,6 @@ import style from "./Cell.module.scss"
 export const Cell = (props)=>{
     var cell = props.cell;
     return(
-        <div onClick={()=>props.scaleUp(cell.id)} className={style.cell} style={{gridArea:`${cell.cs} / ${cell.rs} / ${cell.ce} / ${cell.re}`}}></div>
+        <div onClick={()=>props.scaleUp(cell.id)} className={style.cell} ></div>
     )
 }
--- src/Containers/Screens/Screens.jsx
+++ src/Containers/Screens/Screens.jsx
@@ -1,64 +1,107 @@
-import React from "react";
-import style from "./Screens.module.scss"
-import {Cell} from "../../Components/Cell/Cell";
-export default class Screens extends React.Component{
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AutoResponsive from 'autoresponsive-react';
+import style from './Screens.module.scss'
+export default class Screens extends React.Component {
     constructor(props) {
         super(props);
-        this.state={
-            styles: [style.screens4x4,style.screens1x4x5x6,style.screens2x6x6,style.screens1x6x4x5, style.screen2x2, style.screen2x3],
-            style:5,
-            changeScreenAnim:null,
-            count:1,
-            cells : [{id:1,cs:1,rs:1,ce:19,re:36}]
-        }
+        this.state = {
+            data:[]
+        };
     }
 
     componentDidMount() {
+        this.setState({
+            containerWidth: document.body.clientWidth,
+            containerHeight:document.body.clientHeight,
+        });
+        window.addEventListener('resize', () => {
+            this.setState({
+                containerWidth: ReactDOM.findDOMNode(this.refs.container).clientWidth,
+                containerHeight:ReactDOM.findDOMNode(this.refs.container).clientHeight,
+            });
+        }, false);
 
     }
 
-    addElement = ()=>{
-        let cells = this.state.cells;
-        cells.push(this.generateElem());
-        this.setState({cells:cells});
-        this.setState({count:this.state.count+1})
+    addItem = ()=>{
+        let data = this.state.data;
+        let newItem = {id:data.length+1,size:1,width:this.state.containerWidth/4,height:this.state.containerHeight/4};
+        data.push(newItem);
+        let sElems = data.reduce((a, b)=>a + (b.height*b.width), 0);
+        let sScreen = this.state.containerHeight*this.state.containerWidth;
+        if(sElems>sScreen){
+            return;
+        }
+        this.setState({data:data})
     }
 
-    generateElem = ()=>{
-        let lastCell = this.state.cells.find(i=>i.id===this.state.count);
-        let id = this.state.count+1;
-        let rs = lastCell.re <144 ? lastCell.re : 1;
-        let re = lastCell.re <144 ? lastCell.re +36 : 36;
-        let cs = lastCell.re <144 ? lastCell.cs : lastCell.ce;
-        let ce = lastCell.re <144 ? lastCell.ce : lastCell.ce+19;
-        console.log(lastCell.re,"-------",lastCell.ce);
-        return {id,cs,rs,ce,re};
+    dropItem = (el,id)=>{
+        let data = [].concat(this.state.data);
+        let item = data.find(i=>i.id===id);
+        data.splice(data.indexOf(item),1);
+        this.setState({data:data})
+        el.preventDefault();
+        el.stopPropagation();
+    }
+    getAutoResponsiveProps() {
+        return {
+            itemMargin: 0,
+            containerWidth: this.state.containerWidth ,
+            containerHeight:this.state.containerHeight -21 ,
+            itemClassName: 'item',
+            gridWidth: 8,
+            transitionDuration: '.5'
+        };
     }
 
-    changeGrid = ()=>{
-        this.setState({changeScreenAnim:null})
-        this.setState({changeScreenAnim:style.change_screen})
-    }
+    onChange=(id)=>{
+        let data = JSON.parse(JSON.stringify(this.state.data));
 
-    cellScaleUp = (id)=>{
-        let cells = this.state.cells;
-        let cell = cells.find(i=>i.id===id);
-        cell.ce*=2;
-        cell.re*=2;
-        this.setState({cells:cells})
+        let item = data.find(i=>i.id===id);
+        data.splice(data.indexOf(item),1);
+        if(item.size===1) {
+            item.width *= 2;
+            item.height *= 2;
+            item.size = 2;
+            data.unshift(item);
+        }else{
+            item.width /= 2;
+            item.height /= 2;
+            item.size = 1;
+            data.push(item);
+        }
+        let sElems = data.reduce((a, b)=>a + (b.height*b.width), 0);
+        let sScreen = this.state.containerHeight*this.state.containerWidth;
+        if(sElems>sScreen){
+            data=null;
+            return;
+        }
+
+        this.setState({data:data})
     }
 
     render() {
-        return(
-            <>
-                <button onClick={()=>this.addElement()}>add elem</button>
-                <div onAnimationEnd={()=>this.setState({changeScreenAnim:null})} onClick={()=>this.changeGrid()} className={`${style.screen} ${this.state.styles[this.state.style]} ${this.state.changeScreenAnim}`}>
-                    {this.state.cells.map((item,i)=>{
-                        return <Cell scaleUp={this.cellScaleUp} cell={item} key={i}/>
-                    })}
-                </div>
-            </>
-        )
+        return (
+            <div className={style.screen}>
+                <button onClick={()=>this.addItem()}>ADD</button>
+                <AutoResponsive ref="container" {...this.getAutoResponsiveProps()}>
+                    {
+                        this.state.data.map((i, index) => {
+                            let styleSize = {
+                                width: i.width,
+                                height: i.height
+                            };
+                            return (
+                                <div onClick={()=>this.onChange(i.id)} key={index} href="#" className={`item ${style.cell}`} style={{...styleSize}}>
+                                    <span onClick={(e)=>this.dropItem(e,i.id)}>x</span>
+                                    <p className="a-mask">{i.id}</p>
+                                </div>
+                            );
+                        })
+                    }
+                </AutoResponsive>
+            </div>
+        );
     }
-
 }
